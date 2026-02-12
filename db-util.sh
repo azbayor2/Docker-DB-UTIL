f10b4c0dd6fee54b05d7dd18561359d48f8f4f1c
azbayor2 <hanharry2@gmail.com>
2026-02-12 17:19:10 +0900

    initial commit

---
+++ db-util.sh
@@ -0,0 +1,182 @@
+#!/bin/bash
+#
+#먼저 인자에 위험한 값이 있는지 확인
+
+mkdir -p ./db-util  &> /dev/null
+
+if [[ "$2" == *"/"* ]]; then
+        echo "there should be no path in the second argument"
+        exit 1
+fi
+
+
+# config 파일 확인
+
+if [[ ! -f "./.db-util/config"]]; then  
+    echo "no config file!"
+    exit 1
+
+fi
+
+
+## 환경 변수 가져오기
+source ./.db-util/config
+
+if [[ "$DB_PATH" == "" ]]
+    echo "DB_PATH variable is emtpy!"
+    exit 1
+
+fi
+
+
+# 옵션이 없으면
+if [[ -z "$1" ]]; then
+        echo "인자를 입력해주세요!"
+        exit 1
+
+elif [[ "$1" == "backup" ]]; then   #백업하기
+
+        # 백업저장 폴더 이름 확인하기
+        if [[ -z "$2" ]]; then
+                echo "백업 파일의 이름을 지정해주세요!"
+                exit 1
+        fi
+
+        if [[ -d "./db-util/$2" ]]; then
+                echo "백업 이미 파일이 존재합니다!"
+                exit 1
+
+        fi
+
+
+        if [[ -d "./mysql" ]]; then   #백업할 파일이 있으면
+                sudo cp -r ./mysql "./.db-util/backups/$2" &> /dev/null
+
+
+                #백업 실패
+                if [[ ! -d "./db-util/$2" ]]; then
+                        echo "backup failed.. exiting"
+                        exit 1
+                fi
+
+
+                echo "done backup!"
+                exit 0
+
+        else
+                echo "mysql directory missing!"   #백업할 파일이 없으면
+                exit 1
+        fi
+
+
+elif [[ "$1" == "restore" ]]; then   #복구하기
+
+        #복구 파일 이름 확인하기
+        if [[ -z "$2" ]]; then
+                echo "복구 파일의 이름을 명시해주세요!"
+                exit 1
+        fi
+
+
+        if [[ -d "./db-util/$2" ]]; then   #복구 파일이 있으면
+
+                if [[ ! -f "./docker-compose.yml" ]]; then    #도커 파일이 없으면
+                        echo "docker compose file missing! exiting"
+                        exit 1
+                fi
+
+                echo "turning off containers..."
+                sudo docker compose down -v &> /dev/null   #컨테이너 끄기
+
+                echo "restoring..."
+
+                sudo rm -rf ./mysql &> /dev/null
+                if [[ -d "./mysql" ]]; then   #지우기 실패하면
+                        echo "restore failed.. exiting"
+                        exit 1
+
+                fi
+
+
+                sudo cp -r "./.db-util/backups/$2" ./mysql &> /dev/null
+
+                if [[ ! -d "./mysql" ]]; then
+                        echo "restore failed... exiting"
+                        exit 1
+
+                fi
+
+                echo "turning on containers..."
+                sudo docker compose up -d --build &> /dev/null
+                echo "done restore!"
+                exit 0
+
+        else
+                echo "backup file missing!"
+                echo "please backup first!"
+
+                exit 1
+
+        fi
+
+
+elif [[ "$1" == "reset" ]]; then
+        read -p "are you sure to reset? y/n: " args
+
+        if [[ ! -f "./docker-compose.yml" ]]; then
+                echo "docker compose file missing!"
+                exit 1
+
+        fi
+
+        if [[ "$args" == "y" || "$args" == "Y" ]]; then
+                echo "turning off containers..."
+                sudo docker compose down -v &> /dev/null
+                echo "erasing files.."
+                sudo rm -rf ./mysql &> /dev/null
+                echo "starting containers..."
+                sudo docker compose up -d --build
+
+                echo "done!"
+                exit 0
+
+
+
+        else
+                echo "cancelled!"
+                exit 1
+
+        fi
+
+elif [[ "$1" == "dockerup" ]]; then
+        sudo docker compose up -d --build
+        exit 0
+
+elif [[ "$1" == "dockerdown" ]]; then
+        sudo docker compose down -v
+        exit 0
+
+elif [[ "$1" == "list" ]]; then
+        ls ./db-util/
+        exit 0
+
+elif [[ "$1" == "setpath" ]]; then
+    echo "DB_PATH=$2" > ./.db-util/config
+    echo "done!"
+    exit 0
+
+
+elif [[ "$1" == "options" ]]; then
+        echo "backup [name] - backup mysql container data with specified name"
+        echo "restore [name] - restore mysql container data with specified name"
+        echo "reset - reset mysql container data"
+        echo "dockerup - start containers of docker compose file"
+        echo "dockerdown - remove containes of docker compose file"
+        echo "list - list all available backup files"
+        echo "setpath - set path for backup"
+
+else
+
+        echo "please enter an valid argument!"
+        exit 1
+fi
